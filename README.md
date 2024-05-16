2cc4cdfd4d7965fa1c3959fadf81e8548a797c49
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-05-16 13:23:26 +0100

    Commit all project files to add them to github

---
+++ README.md
@@ -0,0 +1,7 @@
+#Guessing Game
+
+>I've established this project Thursday 06 May 2024
+
+*Number Of Code Lines* : **27**
+
+
---
+++ guessinggame.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+	
+function guessingname(){
+	local count=0
+	while [[ $count -eq 0 ]]
+	do
+		read -p "Try to guess the number of the files on the repository: " guess
+		if [[ $guess -eq 2 ]]
+		then
+			echo "Congratulations that the correct number"
+			count=$((count+1))
+		elif [[ $guess -lt 2 ]]
+		then
+			echo "Too low"
+		elif [[ $guess -gt 2 ]]
+		then
+			echo "Too high"
+		fi
+		
+	done
+}
+
+echo "Welcome back to the game: "
+echo ""
+guessingname
+echo "See you again !!!"
+echo "End of the game"
